b1a09f9bb25d4160a84a23a311581d149e85891f
Sue Burt <sookburt@gmail.com>
2019-01-31 17:58:27 +0000

    Changing from kitty image terminology to camera setings terminology and renaming variables.  Adding input validation.

--- UI/Scripts/script.js
+++ UI/Scripts/script.js
@@ -1,63 +1,116 @@
-﻿const xArray = [100, 200, 400, 800, 1600, 3200, 6400];
-const yArray = ["2.8", "4", "5.6", "8", "11", "16", "22"];
+﻿// TODO: value testing for number does not work...
+// TODO: change start and end values in html for each slider as wrong.
+// TODO: wider container div as value not on same line as slider.
+// TODO: make cat image central.
+// TODO: maybe style the sliders if possible as they're pretty ugly!
+
+const apertureArray = ["2.8", "4", "5.6", "8", "11", "16", "22"]; // x
+const shutterArray = [2000, 1000, 500, 250, 125, 60, 30]; // y
+const isoArray = [100, 200, 400, 800, 1600, 3200, 6400]; // z
 
 // get range objects
-var xDimension = document.getElementById("x-dimension");
-var yDimension = document.getElementById("y-dimension");
-var zDimension = document.getElementById("z-dimension");
+var aDimension = document.getElementById("a-dimension");
+var sDimension = document.getElementById("s-dimension");
+var iDimension = document.getElementById("i-dimension");
 
 // get display objects
-var xDisplay = document.getElementById("currentX");
-var yDisplay = document.getElementById("currentY");
-var zDisplay = document.getElementById("currentZ");
+var aDisplay = document.getElementById("currentA");
+var sDisplay = document.getElementById("currentS");
+var iDisplay = document.getElementById("currentI");
+
 var newUrl = document.getElementById("newUrl");
 
 // get image box
 var imageBox = document.getElementById("image");
 
 // create default settings
-var kittyX = 300;
-var kittyY = 250;
-var kittyUrl = "http://placekitten.com/g/" + kittyX + "/" + kittyY;
-newUrl.innerHTML = kittyUrl;
+var aperture = 300;
+var shutter = 250;
+var iso = 100;
+var imageUrl = "http://placekitten.com/g/" + aperture + "/" + shutter;
+newUrl.innerHTML = imageUrl;
 
 // set x and y displays to default.
-var xValue = xDimension.value;
-xDisplay.innerHTML = "X: " + xValue;
-var yValue = yDimension.value;
-yDisplay.innerHTML = "Y: " + yValue;
-var zValue = zDimension.value;
-zDisplay.innerHTML = "Z: " + zValue;
-var zOpacity = 1;
+var aValue = aDimension.value;
+aDisplay.innerHTML = "F-stop (aperture): " + getValue(apertureArray, aValue);
+var sValue = sDimension.value;
+sDisplay.innerHTML = "Shutter speed: " + getValue(shutterArray, sValue);
+var iValue = iDimension.value;
+iDisplay.innerHTML = "ISO: " + getValue(isoArray, iValue);
+//var zOpacity = 1;
 
 // when x range changes, change the display and the kitty x dimension.
-xDimension.addEventListener("change", function () {
-    xValue = xDimension.value;
+aDimension.addEventListener("change", function () {
+
+    aValue = aDimension.value;
+    var apertureValue = -1;
+
     // check value is an int between 0 and 7 otherwise hack attempt
-    xDisplay.innerHTML = "X: " + xValue;
-    kittyX += xValue * 10;
-    changeImage(kittyUrl);
+    if (typeof aValue !== 'number' || aValue < 0 || aValue > 7) {
+        apertureValue = 0;
+    }
+    else {
+        apertureValue = getValue(apertureArray, aValue);
+    }
+
+    aDisplay.innerHTML = "F-stop (aperture): " + apertureValue;
+    aperture = apertureValue;
+    changeImage(imageUrl);
+
 });
 
-yDimension.addEventListener("change", function () {
-    yValue = yDimension.value;
+sDimension.addEventListener("change", function () {
+
+    sValue = sDimension.value;
+    var shutterValue = -1;
+
     // check value is an int between 0 and 7 otherwise hack attempt
-    yDisplay.innerHTML = "Y: " + yValue;
-    kittyY += yValue * 10;
-    changeImage(kittyUrl);
+    if (typeof sValue !== 'number' || sValue < 0 || sValue > 7) {
+        shutterValue = "000";
+    }
+    else {
+        console.log(sValue);
+        shutterValue = getValue(shutterArray, aValue);
+        console.log(shutterValue)
+    }
+
+    sDisplay.innerHTML = "Shutter speed: " + shutterValue;
+    shutter = shutterValue;
+    changeImage(imageUrl);
+
 });
 
-zDimension.addEventListener("change", function () {
-    var zDim = zDimension.value;
+iDimension.addEventListener("change", function () {
+
+    iValue = iDimension.value;
+    var isoValue = -1;
+
     // check value is an int between 0 and 7 otherwise hack attempt
-    zOpacity = zDim / 10;
-    zDisplay.innerHTML = "Z: " + zOpacity;
-    changeImage(kittyUrl);
+    if (typeof iValue !== 'number' || iValue < 0 || iValue > 7) {
+        isoValue = 0;
+    }
+    else {
+        isoValue = getValue(isoArray, aValue);
+    }
+
+    iDisplay.innerHTML = "ISO: " + isoValue;
+    iso = isoValue;
+    changeImage(imageUrl);
+
 });
 
 function changeImage() {
-    kittyUrl = "http://placekitten.com/g/" + kittyX + "/" + kittyY;
-    newUrl.innerHTML = kittyUrl;
-    imageBox.style.backgroundImage = "url('" + kittyUrl + "')";
-    imageBox.style.opacity = zOpacity;
+
+    //kittyUrl = "http://placekitten.com/g/" + kittyX + "/" + kittyY;
+    newUrl.innerHTML = imageUrl;
+    imageBox.style.backgroundImage = "url('" + imageUrl + "')";
+
+}
+
+function getValue(array, index) {
+    console.log("In getArray function");
+    console.log(array);
+    console.log(index);
+    console.log(array[index]);
+    return array[index];
 }
