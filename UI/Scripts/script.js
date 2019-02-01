f83b5f67ec3a8cd1d973d014ccaabfc153c15075
Sue Burt <sookburt@gmail.com>
2019-02-01 18:34:33 +0000

    Changing to use ISO, shutter speed and aperture values and getting it working properly

--- UI/Scripts/script.js
+++ UI/Scripts/script.js
@@ -1,8 +1,8 @@
-﻿// TODO: value testing for number does not work...
-// TODO: change start and end values in html for each slider as wrong.
-// TODO: wider container div as value not on same line as slider.
-// TODO: make cat image central.
+﻿// TODO: change the float to use grid or flexbox.
 // TODO: maybe style the sliders if possible as they're pretty ugly!
+//https://css-tricks.com/value-bubbles-for-range-inputs/
+// TODO: placekitten no longer works with the dimensions I need - time to create my own images.
+// TODO: check that the slider will work in touch screen and style to allow fat-finger access.
 
 const apertureArray = ["2.8", "4", "5.6", "8", "11", "16", "22"]; // x
 const shutterArray = [2000, 1000, 500, 250, 125, 60, 30]; // y
@@ -18,7 +18,7 @@ var aDisplay = document.getElementById("currentA");
 var sDisplay = document.getElementById("currentS");
 var iDisplay = document.getElementById("currentI");
 
-var newUrl = document.getElementById("newUrl");
+var newUrlDisplay = document.getElementById("newUrl");
 
 // get image box
 var imageBox = document.getElementById("image");
@@ -28,7 +28,7 @@ var aperture = 300;
 var shutter = 250;
 var iso = 100;
 var imageUrl = "http://placekitten.com/g/" + aperture + "/" + shutter;
-newUrl.innerHTML = imageUrl;
+newUrlDisplay.innerHTML = imageUrl;
 
 // set x and y displays to default.
 var aValue = aDimension.value;
@@ -43,15 +43,12 @@ iDisplay.innerHTML = "ISO: " + getValue(isoArray, iValue);
 aDimension.addEventListener("change", function () {
 
     aValue = aDimension.value;
-    var apertureValue = -1;
+    console.log(aValue);
+    var number = validateInput(aValue);
 
-    // check value is an int between 0 and 7 otherwise hack attempt
-    if (typeof aValue !== 'number' || aValue < 0 || aValue > 7) {
-        apertureValue = 0;
-    }
-    else {
-        apertureValue = getValue(apertureArray, aValue);
-    }
+    console.log(number);
+    var apertureValue = getValue(apertureArray, number);
+    console.log(number)
 
     aDisplay.innerHTML = "F-stop (aperture): " + apertureValue;
     aperture = apertureValue;
@@ -62,18 +59,13 @@ aDimension.addEventListener("change", function () {
 sDimension.addEventListener("change", function () {
 
     sValue = sDimension.value;
-    var shutterValue = -1;
 
-    // check value is an int between 0 and 7 otherwise hack attempt
-    if (typeof sValue !== 'number' || sValue < 0 || sValue > 7) {
-        shutterValue = "000";
-    }
-    else {
-        console.log(sValue);
-        shutterValue = getValue(shutterArray, aValue);
-        console.log(shutterValue)
-    }
+    var number = validateInput(sValue);
 
+    console.log(number);
+    var shutterValue = getValue(shutterArray, number);
+    console.log(number)
+  
     sDisplay.innerHTML = "Shutter speed: " + shutterValue;
     shutter = shutterValue;
     changeImage(imageUrl);
@@ -83,15 +75,12 @@ sDimension.addEventListener("change", function () {
 iDimension.addEventListener("change", function () {
 
     iValue = iDimension.value;
-    var isoValue = -1;
 
-    // check value is an int between 0 and 7 otherwise hack attempt
-    if (typeof iValue !== 'number' || iValue < 0 || iValue > 7) {
-        isoValue = 0;
-    }
-    else {
-        isoValue = getValue(isoArray, aValue);
-    }
+    var number = validateInput(iValue);
+
+    console.log(number);
+    var isoValue = getValue(isoArray, number);
+    console.log(number)
 
     iDisplay.innerHTML = "ISO: " + isoValue;
     iso = isoValue;
@@ -101,16 +90,25 @@ iDimension.addEventListener("change", function () {
 
 function changeImage() {
 
-    //kittyUrl = "http://placekitten.com/g/" + kittyX + "/" + kittyY;
-    newUrl.innerHTML = imageUrl;
+    imageUrl = "http://placekitten.com/g/" + aperture + "/" + shutter;
+    newUrlDisplay.innerHTML = imageUrl;
     imageBox.style.backgroundImage = "url('" + imageUrl + "')";
-
 }
 
-function getValue(array, index) {
-    console.log("In getArray function");
-    console.log(array);
-    console.log(index);
-    console.log(array[index]);
+// NOTE: returns 0 if fails validation
+function validateInput(input) {
+
+    var number = parseInt(input);
+    
+    if (isNaN(number) || number < 0 || number > 7) {
+        return 0;
+    }
+    else {
+        return number;
+    }
+}
+
+function getValue(array, index) { 
+
     return array[index];
 }
